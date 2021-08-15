68105a8f1121a837f14c8e8e362abd96ce782955
RedMadda <maddalenarossaro91@gmail.com>
2021-08-15 16:05:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      url: "https://cdn.pixabay.com/photo/2021/07/11/10/39/fantasy-6403406_1280.jpg"
+    }
+});
